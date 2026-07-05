e44ff3b5fff9f315e474a75624c8e5615f4733cc
strawberry-p <strawberry-other-laptop@example.com>
2026-07-05 06:45:23 +0200

    drawing "buckets"

--- main.py
+++ main.py
@@ -25,12 +25,18 @@ def find_tri_pos(width: int, layers: int) -> tuple[int,list[tuple], list[int | f
 def edit_background(bg,pin_pos: list[tuple]):
     unit = TRI_WIDTH/(TRI_LAYERS+1)
     width = round(TRI_WIDTH)
-    height = round((width+unit)*Y_RATIO,1)
+    height = round((width+2*unit)*Y_RATIO,1)
     startx = TRI_OFFSET
     starty = TRI_Y_OFFSET-unit*Y_RATIO
     corners = [(startx,starty), (startx,starty+height), (startx+width,starty+height), (startx+width, starty)]
+    pos = [corners[1][0]+unit,corners[1][1]]
+    tar = [pos[0],pos[1]-Y_RATIO*unit*2]
+    for i in range(9):
+        pygame.draw.line(bg,"brown",pos,tar,3)
+        pos[0] += unit
+        tar[0] += unit
     for i in range(4):
-        pygame.draw.line(bg,"brown",corners[3-i],corners[2-i],3)
+        pygame.draw.line(bg,"brown",corners[3-i],corners[2-i],4)
     for i in pin_pos:
         pygame.draw.circle(bg, "brown", i, 6)
 
@@ -54,7 +60,7 @@ def main():
     d_pos = [x-(6+7+12) for x in depth_pos]
     pygame.init()
     sizing = TRI_WIDTH+TRI_OFFSET*2
-    screen = pygame.display.set_mode((sizing, sizing-TRI_OFFSET))
+    screen = pygame.display.set_mode((sizing, sizing-TRI_OFFSET/2))
     bg = pygame.Surface(screen.get_size())
     bg = bg.convert()
     edit_background(bg, pin_pos)
@@ -63,15 +69,21 @@ def main():
     clock = pygame.time.Clock()
     clock.tick(10)
     #interpolate(screen,clock,100, 80, 400, 70, 100, 5)
-    ly = d_pos.pop(0)
+    start_y = d_pos.pop(0)
+    ly = 0
+    ly += start_y
     lx = TRI_OFFSET+TRI_WIDTH/2
+    bucket = 5
     for y in d_pos:
         c = r.randint(0,1)
         if c == 0: c = -1
+        bucket += c
         tx = lx+c*unit/2
         interpolate(screen,clock,lx,ly,tx,y,10,5)
         ly = y
         lx = tx
+    bucket /= 2
+    print(f"reached {bucket}")
     interpolate(screen, clock, lx, ly, lx, ly+unit*Y_RATIO,10,5)
     running = True
     while running:
